f8d068a10324edd5f90a1b544573253a2ec80cc4
DiegodOliveira <112182138+DiegodOliveira@users.noreply.github.com>
2022-09-15 20:33:58 -0300

    Add files via upload

---
+++ Questão_6_aula_6.py
@@ -0,0 +1,9 @@
+lista_convidados = ['Dicaprio','De Niro','James Hetfield','Van Gogh','Angelina Jolie']
+
+x = 0
+
+while x < len(lista_convidados):
+    print(f"{lista_convidados[x].upper()},venha a festa")
+    x += 1
+
+print("Brad Pitt, não poderá vir, logo, Angelina jolie veio no seu lugar ")
