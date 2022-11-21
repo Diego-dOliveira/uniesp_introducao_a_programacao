3987b5307071ae865896febd7a34138304b391ff
DiegodOliveira <112182138+DiegodOliveira@users.noreply.github.com>
2022-11-21 08:32:33 -0300

    Add files via upload

---
+++ matriz10x10.py
@@ -0,0 +1,17 @@
+from random import randint
+
+matrizA = []
+matrizB = []
+
+for linha in range(10):
+    linha = []
+
+    for coluna in range(10):
+        linha.append(randint(0, 10000))
+
+    matrizA.append(linha)
+
+for linha_matriz in matrizA:
+    print(linha_matriz)
+    for i in linha_matriz:
+        print(i*3)
